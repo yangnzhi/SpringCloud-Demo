b248931410648d2395d6f981cd8033edd48365f6
yangnzhi <yangnzhi@163.com>
2019-01-22 23:24:29 +0800

    update provider_8001

--- 01springcloud-code/microservicecloud/microservicecloud-api/src/main/java/com/youngnzhi/springcloud/entities/Dept.java
+++ 01springcloud-code/microservicecloud/microservicecloud-api/src/main/java/com/youngnzhi/springcloud/entities/Dept.java
@@ -35,4 +35,37 @@ public class Dept implements Serializable {
 
     //来自那个数据库，因为微服务架构可以一个服务对应一个数据库，同一个信息被存储到不同数据库
     private String db_source;
+
+    public Long getDeptno() {
+        return deptno;
+    }
+
+    public void setDeptno(Long deptno) {
+        this.deptno = deptno;
+    }
+
+    public String getDname() {
+        return dname;
+    }
+
+    public void setDname(String dname) {
+        this.dname = dname;
+    }
+
+    public String getDb_source() {
+        return db_source;
+    }
+
+    public void setDb_source(String db_source) {
+        this.db_source = db_source;
+    }
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "Dept{" +
+                "deptno=" + deptno +
+                ", dname='" + dname + '\'' +
+                ", db_source='" + db_source + '\'' +
+                '}';
+    }
 }
