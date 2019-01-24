9750b72e4056990c44f8e2ad56ac3963a6c59d86
yangnzhi <yangnzhi@163.com>
2019-01-24 15:11:07 +0800

    update

--- 01springcloud-code/microservicecloud/microservicecloud-consumer-dept-80/src/main/java/com/youngnzhi/springcloud/controller/DeptController_Consumer.java
+++ 01springcloud-code/microservicecloud/microservicecloud-consumer-dept-80/src/main/java/com/youngnzhi/springcloud/controller/DeptController_Consumer.java
@@ -30,6 +30,7 @@ public class DeptController_Consumer {
         return restTemplate.getForObject(REST_URL_PREFIX+"/dept/get/"+id,Dept.class);
     }
 
+    @RequestMapping("/consumer/list")
     public List<Dept> list() {
         return restTemplate.getForObject(REST_URL_PREFIX+"/dept/list",List.class);
     }
