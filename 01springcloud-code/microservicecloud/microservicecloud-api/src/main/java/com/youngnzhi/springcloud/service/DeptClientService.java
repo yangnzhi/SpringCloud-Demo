3cd304911ccbde6747a3e9bbce10e440ece6551d
yangnzhi <yangnzhi@163.com>
2019-04-28 16:19:00 +0800

    update

--- 01springcloud-code/microservicecloud/microservicecloud-api/src/main/java/com/youngnzhi/springcloud/service/DeptClientService.java
+++ 01springcloud-code/microservicecloud/microservicecloud-api/src/main/java/com/youngnzhi/springcloud/service/DeptClientService.java
@@ -15,7 +15,8 @@ import java.util.List;
  * @FeignClient feign负载均衡声明式注解，
  *    value:为微服务名称，针对这个微服务细腻化feign负载均衡
  *    fallback:当执行当前微服务中方法或当前微服务所在服务器出现异常时
- *             调用DeptClientServiceFallbackFactory类返回配置好的备选响应信息给前台，而不是返回不可阅读的报错的异常信息或服务器异常
+ *             调用DeptClientServiceFallbackFactory类返回配置好的备选响应信息给前台，
+ *             而不是返回不可阅读的报错的异常信息或服务器异常，
  *             做了服务降级处理，让客户端在服务端不可用时也会获得提示信息而不会挂起耗死服务器
  * @RequestMapping value和服务端映射地址一致
  */
